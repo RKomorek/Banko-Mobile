8ffdda06d886a6bd60444561746340b0ef8720b7
LoracVitoria <carolvitoria1705@gmail.com>
2025-10-06 07:44:57 -0300

    alteração de projeto firebase

--- firebase.ts
+++ firebase.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCDIqrxMzSFkuHY8kNAj6eLIN7k6KC7i10",
-  authDomain: "banko-43675.firebaseapp.com",
-  projectId: "banko-43675",
-  storageBucket: "banko-43675.firebasestorage.app",
-  messagingSenderId: "719378461127",
-  appId: "1:719378461127:web:6d761f236b1b62eea969e2"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
